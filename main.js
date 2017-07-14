847690852b9ebf446c24bf496dd445d041a2dc58
Leonard <leonard.horne@gmail.com>
2017-07-14 14:00:57 -0400

    Add display function. Add final display style once clock hits 0.

---
+++ main.js
@@ -0,0 +1,27 @@
+
+const tick = 1 * 1000;
+let secondsLeft = 3;
+
+let displayEl = document.querySelector('h1')
+
+updateDisplay(secondsLeft);
+
+function updateDisplay(msg) {
+  displayEl.innerHTML = msg;
+}
+
+let intervalID = window.setInterval(subtractFromCountdown, tick);
+
+function subtractFromCountdown() {
+    secondsLeft -= 1;
+    updateDisplay(secondsLeft);
+
+    if (secondsLeft === 0) {
+      window.clearInterval(intervalID);
+      let body = document.querySelector('body')
+      body.style.backgroundColor = 'red';
+      body.style.Color = 'white';
+      updateDisplay('BOOM!');
+    }
+console.log('doing work');
+}
